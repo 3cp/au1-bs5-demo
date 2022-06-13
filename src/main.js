4035fc4ae313036b7b901486e31a1ee07bb116ea
Chunpeng Huo <huocp@me.com>
2022-06-13 11:53:54 +1000

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import environment from './environment';
+import 'bootstrap';
+import '@popperjs/core';
+import process from 'process';
+window.process = process;
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources');
+
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
